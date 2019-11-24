968cab41003bd18565f11a240197fc3f2232a48d
Trahim Soufiane <tr4him@gmail.com>
2019-11-24 15:05:05 +0100

    App required plugins

--- includes/class-woo-crud.php
+++ includes/class-woo-crud.php
@@ -37,10 +37,6 @@ class WooCrudCore {
 	 * Register all of the hooks.
 	 */
     public function enqueue_styles() {
-		//Import bootstrap
-		wp_enqueue_style( $this->plugin_name.'-cdn-vue','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), $this->version, 'all' );
-		//Woo Crud styles
-		wp_enqueue_style( $this->plugin_name.'-main-styles',plugins_url( '../assets/css/main.css', __FILE__ ), array(), $this->version, 'all' );
     }
     
     public function enqueue_scripts() {
@@ -59,51 +55,6 @@ class WooCrudCore {
 	 * Register all admin hooks.
 	 */
 	private function define_admin_hooks() {
-		add_action('admin_menu', array( $this, 'one_viewer_register_menu_settings'));
-		
-	}
-
-	public function one_viewer_register_menu_settings(){
-		add_options_page('Woo Crud', 'Woo Crud', 'manage_options','woo-crud-settings',array( $this, 'woo_crud_admin_template' ));
-	}
-	
-	/**
-	 * Page settings template and logic
-	 */
-	public function woo_crud_admin_template(){
-		/**
-		 * Check if user clicked save on the settings page.
-		 */
-		if ( !empty($_POST)){
-			//Get selected categories.
-			$categories = $_POST['wp-newiewer-select'];
-			//Turn selected categories to string
-			$categoriesToString = trim(implode(',',$categories));
-			//Update setting field with new selected categories
-			update_option( 'oneviewer_categories', $categoriesToString);
-			//Create Message tempalte
-			$HTMLNotice = '<div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible">';
-			$HTMLNotice .= '<p><strong>Settings saved.</strong></p>';
-			$HTMLNotice .= '<button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
-			echo $HTMLNotice;
-		}
-		/**
-		 * Page settings template HTML and Form
-		 */
-		$HTMLTemplate = "";
-		$HTMLTemplate .= '<div class="woo-crud-container">';
-		$HTMLTemplate .= '<h1>WooCrud Settings</h1>';
-		$HTMLTemplate .= '<p>Please choose categories to show on viewer, You can select one or multiple categories</p>';
-		$HTMLTemplate .= '<form action="'.$_SERVER['REQUEST_URI'].'" method="post">';
-		$HTMLTemplate .= '<p>';
-		$HTMLTemplate .= 'HERE WE GO AGAIN';
-		$HTMLTemplate .= '</p>';
-		$HTMLTemplate .= '<p><input name="Submit" type="submit" class="button-primary" value="Save Changes" /></p>';
-		$HTMLTemplate .= '</form>';
-		$HTMLTemplate .= '</div>';
-		
-		echo $HTMLTemplate;
-	
 	}
 
 	/**
--- woo-crud.php
+++ woo-crud.php
@@ -16,17 +16,12 @@ if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');
 define( 'WOOCRUD_VERSION', '1.0.0' );
 define( 'WOOCRUD_REQUIRED_WP_VERSION', '4.9' );
 define( 'WOOCRUD_PLUGIN', dirname( __FILE__ ) );
-
- 
-
+    
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-viewer-per-category-activator.php
  */
 function activate_woo_crud() {
-
-     
- 
 }
 
 /**
@@ -34,7 +29,6 @@ function activate_woo_crud() {
  * This action is documented in includes/class-viewer-per-category-deactivator.php
  */
 function deactivate_woo_crud() {
-
 }
 
 register_activation_hook( __FILE__, 'activate_woo_crud' );
@@ -45,17 +39,76 @@ register_deactivation_hook( __FILE__, 'deactivate_woo_crud' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
+require plugin_dir_path( __FILE__ ) . 'includes/class-tgm-plugin-activation.php';
 require plugin_dir_path( __FILE__ ) . 'includes/class-endpoints.php';
-
 require plugin_dir_path( __FILE__ ) . 'includes/class-woo-crud.php';
 
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register five plugins:
+ * - one included with the TGMPA library
+ * - two from an external source, one from an arbitrary source, one from a GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+ *
+ * The variables passed to the `tgmpa()` function should be:
+ * - an array of plugin arrays;
+ * - optionally a configuration array.
+ * If you are not changing anything in the configuration array, you can remove the array and remove the
+ * variable from the function call: `tgmpa( $plugins );`.
+ * In that case, the TGMPA default settings will be used.
+ *
+ * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+ */
+function woo_crud_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'JWT Authentication for WP-API',
+			'slug'      => 'jwt-authentication-for-wp-rest-api',
+			'required'  => true,
+        ),
+        
+		array(
+			'name'      => 'WooCommerce',
+			'slug'      => 'woocommerce',
+			'required'  => true,
+		),
 
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'woo-crud',               // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                       // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins',  // Menu slug.
+		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+		'capability'   => 'manage_options',         // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                     // Show admin notices or not.
+		'dismissable'  => true,                     // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                       // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                    // Automatically activate plugins after installation or not.
+		'message'      => '',                       // Message to output right before the plugins table.
+	);
+	tgmpa( $plugins, $config );
+}
  
 /* 
-    One Viewer Run
+    Woo Crud Run
 */
 function wp_woo_crud_run () {
-
     
     $urlsInit = new WooCrudEndPoint();
     $urlsInit->Register_Endpoint();
@@ -63,11 +116,7 @@ function wp_woo_crud_run () {
 	$plugin = new WooCrudCore();
     $plugin->init();
 
+    add_action( 'tgmpa_register', 'woo_crud_register_required_plugins' );
+
 }
 wp_woo_crud_run ();
-
-
-
-
-
- 
